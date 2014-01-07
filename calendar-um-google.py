e87b34702c503f1797c9ddc1fe1048b227476fa3
ByzanTine <morbyzantine@gmail.com>
2014-01-07 15:38:07 -0500

    python

---
+++ calendar-um-google.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+import HTMLParser  
+import urlparse  
+import urllib  
+import urllib2  
+import cookielib  
+import string  
+import re  
+import sys
+import getpass
+usrname = raw_input("User-Name: ")
+password = getpass.getpass('Password:')
+
+
+
+
+#登录的主页面  
+hosturl = 'https://weblogin.umich.edu/'
+#hosturl = 'https://csprod.dsc.umich.edu/services/student'
+posturl = 'https://weblogin.umich.edu/cosign-bin/cosign.cgi'
+#设置一个cookie处理器，它负责从服务器下载cookie到本地，并且在发送请求时带上本地的cookie  
+cj = cookielib.LWPCookieJar()  
+cookie_support = urllib2.HTTPCookieProcessor(cj)  
+opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)  
+urllib2.install_opener(opener)  
+  
+#打开登录主页面（他的目的是从页面下载cookie，这样我们在再送post数据时就有cookie了，否则发送不成功）  
+h = urllib2.urlopen(hosturl)  
+#print h.read()
+#print cj
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+
+			}
+postData = {'ref':'',
+			'service':'',
+			'required':'',
+			'login':usrname,
+			'password':password,
+			'tokencode': ''
+            }  
+postData = urllib.urlencode(postData)  
+request = urllib2.Request(posturl, postData, headers)  
+
+response = urllib2.urlopen(request)
+#print   response.code
+
+text = response.read() 
+# print '************************************************************************************'
+# print text  
+# check if authenticated
+# print '************************************************************************************'
+#print cj
+
+content= urllib2.urlopen('https://csprod.dsc.umich.edu/services/student')
+#print content.geturl()
+schedule= urllib2.urlopen('https://csprod.dsc.umich.edu/psc/csprodnonop/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL')
+#print schedule.read()
+# Open a file
+fo = open("output", "wb")
+fo.write( schedule.read());
+
+# Close opend file
+fo.close()
